45eeaf1220b93619e5763f27720c6b23ae35a518
TomohiroKinoshita <t.kinoshita922@gmail.com>
2020-07-01 02:12:47 +0900

    (7)タスク編集可能実装完了

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
-use App\Task; // ★ 追加え
+use App\Task; // ★ 追加
+use App\Http\Requests\EditTask;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateTask;
 
@@ -56,6 +57,23 @@ class TaskController extends Controller
 
     return view('tasks/edit', [
         'task' => $task,
+    ]);
+}
+
+public function edit(int $id, int $task_id, EditTask $request)
+{
+    // 1
+    $task = Task::find($task_id);
+
+    // 2
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    // 3
+    return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
     ]);
 }
 }
